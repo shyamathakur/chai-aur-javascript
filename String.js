89d28b77c2edf1564817120171e577e0cc7d43df
shyamathakur <shyamathakur406@gmail.com>
2023-10-03 21:41:04 +0530

    first commit

---
+++ String.js
@@ -0,0 +1,8 @@
+// const newString = new String ("shyama")
+const newString = "shyama"
+
+const name = newString.substring(0,4)
+const w = newString.slice(0,3)
+
+// console.log(name);
+console.log(newString.split(0,3));
