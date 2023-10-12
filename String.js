d8b31b69fc3975e4f1ddf0c69cd35e6dce414ce3
shyamathakur <shyamathakur406@gmail.com>
2023-10-12 20:21:32 +0530

    message

--- String.js
+++ String.js
@@ -5,4 +5,11 @@ const name = newString.substring(0,4)
 const w = newString.slice(0,3)
 
 // console.log(name);
-console.log(newString.split(0,3));
+// console.log(newString.split(0,3));
+
+
+const url = "https://shyama.com20%thakur";
+
+// console.log(url.replace("20%","-"));
+
+console.log(url.includes("shyama"));
---
+++ Variable.js
@@ -0,0 +1,16 @@
+const accoundId = 1234;
+let userName = "shyama"
+var password = "ijfe12"
+city = "indore"
+let userClass; //value na dene pr result undefined ayega
+
+// accoundId = 12; // it's block scope so you can not change value
+userName = "thakur"
+password = "23232"
+city = "jaipur"
+
+
+
+
+
+console.table([accoundId, userName, password,city, userClass]); // console.table use karne pr value table ke ander ayegi 
